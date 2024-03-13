7997387716618f3a1f736c18b846c656ee7397c6
Dileep Pandey <dileep9594@gmail.com>
2024-03-13 15:29:39 +0530

    project intialization

---
+++ LeaderBoard/src/test/java/com/leaderboard/LeaderBoard/LeaderBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaderboard.LeaderBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
